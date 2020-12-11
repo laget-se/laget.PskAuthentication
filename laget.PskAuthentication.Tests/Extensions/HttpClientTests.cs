e9308bc5d9632376a861bbc7d55ca903a3ea2cab
Erik Hennerfors <erik.hennerfors@laget.se>
2020-12-11 20:03:48 +0100

    Added HttpClient extension tests

---
+++ laget.PskAuthentication.Tests/Extensions/HttpClientTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using FluentAssertions;
+using laget.PskAuthentication.Client.Extensions;
+using laget.PskAuthentication.Core;
+using Xunit;
+
+namespace laget.PskAuthentication.Tests.Extensions
+{
+    public class HttpClientTests
+    {
+        readonly PskAuthenticationOptions _options;
+
+        public HttpClientTests()
+        {
+            _options = new PskAuthenticationOptions
+            {
+                RijndaelIV = "fWN4n4pXsrXSJCdN9HfjiA==",
+                RijndaelKey = "J/IsWTGD5Sx2B124mtDg0Pg8AGslPADgGgiOj0kfxh0=",
+                Salt = "fenga1N3BYtRzwV9",
+                Secret = "Banana"
+            };
+        }
+
+
+        [Fact]
+        public void ShouldAddPskAuthentication()
+        {
+            var client = new HttpClient().AddPskAuthentication(_options);
+
+            client.DefaultRequestHeaders.Contains("X-PSK-Authorization").Should().BeTrue();
+        }
+    }
+}
