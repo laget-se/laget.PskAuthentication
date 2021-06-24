bf9b7d6cb07c3cbe2c37e8a22baeb7305961079a
Erik Hennerfors <erik.hennerfors@obscured.se>
2021-06-24 12:19:45 +0200

    Added Generator to generate PSK Token for e.g. dev env.

---
+++ laget.PskAuthentication.Generator/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+
+namespace laget.PskAuthentication.Generator
+{
+    internal class Program
+    {
+        private static async Task Main(string[] args)
+        {
+            await Host.CreateDefaultBuilder()
+                //.ConfigureContainer<ContainerBuilder>((context, builder) =>
+                //{
+                //    builder.RegisterModule<RepositoryModule>();
+                //    builder.RegisterModule<ServiceModule>();
+                //})
+                .ConfigureServices((context, services) =>
+                {
+                    services.AddHostedService<Service>();
+                })
+                .UseConsoleLifetime()
+                .UseEnvironment(Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Development")
+                .UseSerilog((context, services, configuration) => configuration.ReadFrom.Configuration(context.Configuration))
+                //.UseServiceProviderFactory(new AutofacServiceProviderFactory())
+                .Build()
+                .RunAsync();
+        }
+    }
+}
---
+++ laget.PskAuthentication.Generator/Service.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using laget.PskAuthentication.Client;
+using laget.PskAuthentication.Core;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+
+namespace laget.PskAuthentication.Generator
+{
+    public class Service : IHostedService
+    {
+        private readonly IConfiguration _configuration;
+
+        public Service(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public async Task StartAsync(CancellationToken cancellationToken)
+        {
+            var ttl = 315360000; // 10 years in seconds
+            var options = new PskAuthenticationOptions
+            {
+                RijndaelIV = _configuration.GetValue<string>("Security:RijndaelIV"),
+                RijndaelKey = _configuration.GetValue<string>("Security:RijndaelKey"),
+                Salt = _configuration.GetValue<string>("Security:Salt"),
+                Secret = _configuration.GetValue<string>("Security:Secret"),
+                Ttl = 315360000
+            };
+            var generator = new PskGenerator(options);
+            var psk = generator.Generate();
+
+            Log.Information("PSK:");
+            Log.Information(psk);
+
+            Log.Information("Done!!!");
+        }
+
+        public async Task StopAsync(CancellationToken cancellationToken)
+        {
+            Environment.Exit(0);
+        }
+    }
+}
