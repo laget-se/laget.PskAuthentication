51b19204177455495eca0668a46f50be07109597
Erik Hennerfors <erik.hennerfors@obscured.se>
2022-10-27 09:24:54 +0200

    Added AssemblyInfo.cs to all projects

---
+++ laget.PskAuthentication.Mvc/Properties/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+
+[assembly: AssemblyCompany("Sportion Media Group AB")]
+[assembly: AssemblyConfiguration("Release")]
+[assembly: AssemblyFileVersion("1.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0")]
+[assembly: AssemblyProduct("laget.PskAuthentication.Mvc")]
+[assembly: AssemblyTitle("laget.PskAuthentication.Mvc")]
+[assembly: AssemblyVersion("1.0.0")]
+
+[assembly: InternalsVisibleTo("laget.PskAuthentication.Mvc.Tests")]
